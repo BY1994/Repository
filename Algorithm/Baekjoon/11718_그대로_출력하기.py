a75331e7d55b8cbdff710c39669864541c83fdbf
BY1994 <lovelychoco100@gmail.com>
2019-01-03 18:04:07 +0900

    backjoon 11718 first trial

---
+++ Algorithm/Baekjoon/11718_그대로_출력하기.py
@@ -0,0 +1,16 @@
+# 백준 알고리즘 11718
+# 백준 Online Judge - 문제 - 단계별로 풀어보기 - 입/출력 받아보기 - 그대로 출력하기
+# 최초 풀이 2019.01.03 PBY
+
+# 답안 참고한 사이트 https://chuckolet.tistory.com/4
+
+# 1. 실패한 코드 런타임 에러
+
+# import sys
+# for i in sys.stdin:
+#    pirnt(i, end="")
+
+# 2. 실패한 코드 런타임 에러
+
+# while True:
+#   print(input())
