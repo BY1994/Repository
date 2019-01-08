4e97ad374cacb544d9a80ef8207568438feffb3c
BY1994 <lovelychoco100@gmail.com>
2019-01-08 16:18:52 +0900

    found mistake at print function

--- Algorithm/Baekjoon/11718_그대로_출력하기.py
+++ Algorithm/Baekjoon/11718_그대로_출력하기.py
@@ -1,8 +1,14 @@
 # 백준 알고리즘 11718
 # 백준 Online Judge - 문제 - 단계별로 풀어보기 - 입/출력 받아보기 - 그대로 출력하기
 # 최초 풀이 2019.01.03 PBY
+# 최종 답안 2019.01.08 PBY
 
 # 답안 참고한 사이트 https://chuckolet.tistory.com/4
+
+import sys
+for i in sys.stdin:
+    print(i, end="")
+
 
 # 1. 실패한 코드 런타임 에러
 
@@ -14,3 +20,8 @@
 
 # while True:
 #   print(input())
+
+
+# 2번의 코드는 파이썬에서 동작이 느리기 때문이지만,
+# 1번의 코드는 print에서 오타가 났기 때문인 것을 발견하고
+# 수정하여 제출하였다.
