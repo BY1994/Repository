ffbed2235b5caedce47c73766fbf104934d7be67
BY1994 <lovelychoco100@gmail.com>
2019-07-30 23:36:25 +0900

    Update 7465_창용 마을 무리의 개수.py

--- Algorithm/SWEA/7465_창용 마을 무리의 개수.py
+++ Algorithm/SWEA/7465_창용 마을 무리의 개수.py
@@ -1,9 +1,75 @@
 """
 7465 창용 마을 무리의 개수
 
-2019.07.08 PBY 최초작성
-"""
+input)
+2
+6 5
+1 2
+2 5
+5 1
+3 4
+4 6
+6 8
+1 2
+2 5
+5 1
+3 4
+4 6
+5 4
+2 4
+2 3
 
+2019.07.08 PBY 최초 작성
+2019.07.30 PBY 다시 작성
+"""     
+
+def union_group(ind, N, town):
+    for j in range(N):
+        if town[ind][j] == 1:
+            group[group[0]].extend([i, j])
+            town[ind][j] = 0
+            town[j][ind] = 0
+            union_group(j, N, town)
+
+for testcase in range(int(input())):
+    
+    # input
+    N, M = map(int, input().split())
+    town = [[0]*N for _ in range(N)]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        town[a-1][b-1] = 1
+        town[b-1][a-1] = 1
+
+    # 개인 그룹
+    ans = 0
+    for i in range(N):
+        if sum(town[i][:]) == 0:
+            ans += 1
+            
+    # find group
+    group = [0]
+    for i in range(N):
+        flag = 0
+        for j in range(N):
+            if town[i][j] == 1:
+                flag += 1
+                if flag == 1: # 첫번째 아는 사람이면
+                    group[0] += 1
+                    group.append([i, j])
+                    town[i][j] = 0
+                    town[j][i] = 0
+                else: # 다른 아는 사람이면
+                    group[group[0]].extend([i, j])
+                    town[i][j] = 0
+                    town[j][i] = 0
+                union_group(j, N, town)                    
+            
+    
+    print("#%d %d" %(testcase+1, ans + group[0]))
+
+# 반만 맞음
+"""
 for testcase in range(int(input())):
     ans = 0
     N, M = map(int, input().split())
@@ -40,3 +106,4 @@ for testcase in range(int(input())):
         ans += len(set(city))-1
 
     print("#%d %d" %(testcase+1, ans))
+"""
