a3f9d45ba08d085b1a1322544d37acaab1300280
BY1994 <lovelychoco100@gmail.com>
2019-09-10 21:47:08 +0900

    Update Ensemble_classifiers.py

--- Python/Contest/Ensemble_classifiers.py
+++ Python/Contest/Ensemble_classifiers.py
@@ -8,6 +8,11 @@ from sklearn.metrics import accuracy_score
 # from sklearn.datasets import make_moons
 ​
 # load data (load csv)
+import pandas as pd
+data = pd.read_csv("AFSNT.csv", encoding="cp949")
+# data.head()
+data.drop(['REG', 'IRR', 'ATT', 'DRR', 'CNL', 'CNR'], axis=1, inplace=True)
+
 X, y = make_moons(n_samples=500, noise=0.30, random_state=42)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
