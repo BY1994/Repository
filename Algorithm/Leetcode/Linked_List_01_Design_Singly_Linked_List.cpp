b82c90317183c3e5b075723d23ef3c6c57d76e7a
BY1994 <lovelychoco100@gmail.com>
2021-04-18 15:15:17 +0900

    [Algorithm] Leetcode: update Single Linked List problem
    This patch updated Single Linked List design problem.
    Previous code caused Wrong Answer
    Because of misunderstanding at AddAtIndex function.
    
    Signed-off-by: BY1994 <lovelychoco100@gmail.com>

--- Algorithm/Leetcode/Linked_List_01_Design_Singly_Linked_List.cpp
+++ Algorithm/Leetcode/Linked_List_01_Design_Singly_Linked_List.cpp
@@ -3,6 +3,7 @@
 
 // 고려해야할 사항: 새로운 걸 추가할 때 Head랑 Tail 이 같이 업데이트 되어야함 
 // 주제가 Single Linked List 이므로 prev, next 로 하는 게 아니면 Tail 을 찾아가라는 듯 
+// => 35 ms (beat 95.62 % ) 로 봐서 malloc을 안 쓰고 정적으로 해서 빠른 듯하다 
 
 typedef struct _MyList{
     int val;
@@ -11,7 +12,6 @@ typedef struct _MyList{
 
 typedef struct {
     MyList * Head;
-    //MyList * Tail;
     int buf_ind;
     MyList buf[2021];
 } MyLinkedList;
@@ -22,17 +22,18 @@ MyLinkedList* myLinkedListCreate() {
     MyLinkedList * LinkedList = (MyLinkedList *)malloc(sizeof(MyLinkedList));
     
     LinkedList->Head = &(LinkedList->buf[0]);
-    //LinkedList->Tail = &(LinkedList->buf[0]);
     LinkedList->Head->next = NULL;
-    //LinkedList->Tail->next = NULL;
     LinkedList->buf_ind = 1;
+    
     return LinkedList;
 }
 
+//#include <stdio.h>
 /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
 int myLinkedListGet(MyLinkedList* obj, int index) {
     int current = 0;
     for (MyList *p = obj->Head->next; p; p=p->next) {
+    	//printf("get %d\n", p->val); 
         if (current == index) return p->val;
         current++;
     }
@@ -41,7 +42,6 @@ int myLinkedListGet(MyLinkedList* obj, int index) {
 
 /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
 void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
-	// Head랑 Tail 이랑 같으면 Head 추가할 때 Tail 에도 추가해야함 
     obj->buf[obj->buf_ind].val = val;
     obj->buf[obj->buf_ind].next = obj->Head->next;
     obj->Head->next = &(obj->buf[obj->buf_ind]);
@@ -57,12 +57,6 @@ void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
     obj->buf[obj->buf_ind].next = p->next;
     p->next = &(obj->buf[obj->buf_ind]);
     obj->buf_ind++;
-    
-	// Head랑 Tail 이랑 같으면 Tail 추가할 때 Head에도 추가해야함 
-    //obj->buf[obj->buf_ind].val = val;
-    //obj->buf[obj->buf_ind].next = obj->Tail;
-    //obj->Tail = &(obj->buf[obj->buf_ind]);
-    //obj->buf_ind++;
 }
 
 /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
@@ -70,16 +64,17 @@ void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
     int current = 0;
     MyList *prev = obj->Head;
     for (MyList *p = obj->Head->next; p; p=p->next) {
-        if (current == index) {
-            obj->buf[obj->buf_ind].val = val;
-            obj->buf[obj->buf_ind].next = prev->next;
-            prev->next = &(obj->buf[obj->buf_ind]);
-            obj->buf_ind++;
-            return;
-        }
+        if (current == index) break;
         prev = p;
         current++;
     }
+    
+    if (current == index) {
+        obj->buf[obj->buf_ind].val = val;
+        obj->buf[obj->buf_ind].next = prev->next;
+        prev->next = &(obj->buf[obj->buf_ind]);
+        obj->buf_ind++;
+	}
 }
 
 /** Delete the index-th node in the linked list, if the index is valid. */
@@ -129,5 +124,46 @@ int main(void)
 	printf("%d\n", myLinkedListGet(obj, 1));
 	myLinkedListDeleteAtIndex(obj, 1);
 	printf("%d\n", myLinkedListGet(obj, 1));
+
+// Wrong Answer 4가 나와야하는데 -1이 나옴 
+// 문제에 다음과 같은 조건이 존재함
+//  If index equals the length of the linked list, the node will be appended to the end of the linked list. 
+	MyLinkedList* obj2 = myLinkedListCreate();
+	
+	// 6 1 2 [7] 0 4
+	myLinkedListAddAtHead(obj2, 7);
+	myLinkedListAddAtHead(obj2, 2);
+	myLinkedListAddAtHead(obj2, 1);
+	myLinkedListAddAtIndex(obj2, 3, 0);
+	myLinkedListDeleteAtIndex(obj2, 2);
+	myLinkedListAddAtHead(obj2, 6);
+	myLinkedListAddAtTail(obj2, 4);
+	printf("%d\n", myLinkedListGet(obj2, 4));
+	myLinkedListAddAtHead(obj2, 4);
+	myLinkedListAddAtIndex(obj2, 5, 0);
+	myLinkedListAddAtHead(obj2, 6);
+
 	return 0;
 }
+
+/*
+Implement the MyLinkedList class:
+
+MyLinkedList() Initializes the MyLinkedList object.
+int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+void addAtTail(int val) Append a node of value val as the last element of the linked list.
+void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
+void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
+*/
+
+/*
+Wrong Answer 
+Input:
+["MyLinkedList","addAtHead","addAtHead","addAtHead","addAtIndex","deleteAtIndex","addAtHead","addAtTail","get","addAtHead","addAtIndex","addAtHead"]
+[[],[7],[2],[1],[3,0],[2],[6],[4],[4],[4],[5,0],[6]]
+Output:
+[null,null,null,null,null,null,null,null,-1,null,null,null]
+Expected:
+[null,null,null,null,null,null,null,null,4,null,null,null]
+*/
