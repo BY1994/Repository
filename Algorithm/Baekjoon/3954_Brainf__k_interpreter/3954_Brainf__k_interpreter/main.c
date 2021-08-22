eae9f2dc6fa0e251b6ea02b80e3280c9c527f133
BY1994 <lovelychoco100@gmail.com>
2021-08-22 14:44:14 +0900

    [Algorithm] Baekjoon: update 3954 brain f__k interpreter
    This patch updated 3954.
    This answer also got wrong.
    
    Signed-off-by: BY1994 <lovelychoco100@gmail.com>

--- Algorithm/Baekjoon/3954_Brainf__k_interpreter/3954_Brainf__k_interpreter/main.c
+++ Algorithm/Baekjoon/3954_Brainf__k_interpreter/3954_Brainf__k_interpreter/main.c
@@ -34,6 +34,41 @@ Input
 Output
 Loops 1 123
 Loops 3 4
+4. infinite loop 안에 50000000 번 실행
+terminate 후에도 50000000 번 실행되도록 바꿈
+
+https://www.acmicpc.net/board/view/61124
+Input
+1
+3 209 1
++[>+[>----------------------------------[+++]<+]++++++++++++++++++++++++++[+]<+],[-].+[-[[>+[>----------------------------------[+++]<+]++++++++++++++++++++++++++[+]<+]----------------------------------[+]]++]
+1
+
+Output
+Loops 86 208
+
+5. 무한루프 진입시 조건이 항상 동일하지 않음
+무한루프 [ 로 돌아올 때 조건이 항상 똑같지 않다.
+무조건 ++ 로 무한히 늘어날 수도 있음
+=> 무한루프 체크한 걸 못 찾으면 가장 안쪽 루프에서 탈출 못하는 것
+
+https://www.acmicpc.net/board/view/58310
+===== 입력 =====
+2
+10 5 1
++[[]]
+a
+1 9 1
+++[[++]+]
+a
+
+===== 출력 =====
+Loops 2 3
+Loops 3 6
+
+6. 모든 반례 적용하고도 wrong
+5번에서 무한루프가 항상 조건이 똑같지 않다는게 문제인 듯...
+
 */
 
 #include <stdio.h>
@@ -43,28 +78,30 @@ int csize;
 int isize;
 unsigned int pointer;
 unsigned int input_index;
-unsigned int last_loop; /* last loop [ index */
 
 unsigned char codes[4096];
+unsigned char m_for_codes[4096];
 unsigned char inputs[4096];
 unsigned char memory[100010];
 unsigned char jumps[4096];
 
-int sets[2][4100]; /* 0: [ or ], 1: location */
+int stack[5000];
+int infinite[5000];
+int sp;
+int tc;
 
 void preprocess(void)
 {
-	int num = 0;
+	sp = 0;
 	for (int i = 0; i < csize; i++) {
 		if (codes[i] == '[') {
-			num++;
-			sets[0][num] = i;
+			sp++;
+			stack[sp] = i;
 		}
 		if (codes[i] == ']') {
-			sets[1][num] = i;
-			jumps[sets[0][num]] = sets[1][num];
-			jumps[sets[1][num]] = sets[0][num];
-			num--;
+			jumps[stack[sp]] = i;
+			jumps[i] = stack[sp];
+			sp--;
 		}
 	}
 }
@@ -72,6 +109,7 @@ void preprocess(void)
 int do_code(int index)
 {
 	int ret = index + 1;
+
 	if (codes[index] == '-') {
 		memory[pointer] = (memory[pointer] + 256 - 1) % 256;
 	}
@@ -85,11 +123,23 @@ int do_code(int index)
 		pointer = (pointer + 1) % msize;
 	}
 	else if (codes[index] == '[') {
-		last_loop = index;
-		if (memory[pointer] == 0) ret = jumps[index] + 1;
+		if (memory[pointer] == 0) { /* loop pass */
+			ret = jumps[index] + 1;
+		}
+		else {
+			infinite[sp] = 0;
+			stack[sp] = index;
+			sp++;
+		}
 	}
 	else if (codes[index] == ']') {
-		if (memory[pointer] != 0) ret = jumps[index] + 1;
+		if (memory[pointer] != 0) {
+			ret = jumps[index] + 1;
+			/* same as previous enter */
+			if (m_for_codes[index] == memory[pointer]) infinite[sp-1] = tc;
+			else infinite[sp-1] = 0;
+		}
+		else sp--;
 	}
 	else if (codes[index] == ',') {
 		if (input_index >= isize) {
@@ -99,6 +149,8 @@ int do_code(int index)
 			memory[pointer] = inputs[input_index++];
 		}
 	} // pass "."
+	m_for_codes[index] = memory[pointer];
+
 	return ret;
 }
 
@@ -106,20 +158,33 @@ void run(void)
 {
 	int max_code = 50000001;
 	int index = 0;
+	int i;
 
 	pointer = 0;
 	input_index = 0;
-	last_loop = 0;
+	sp = 0;
 
+	/* terminates within 50 000 000 instructions */
 	while (max_code--) {
 		if (index >= csize) {
 			printf("Terminates\n");
 			return;
 		}
-
 		index = do_code(index);
 	}
-	printf("Loops %d %d\n", last_loop, jumps[last_loop]);
+
+	/* at most 50 000 000 instructions within infinite loop */
+	max_code = 50000001;
+	while (max_code--) index = do_code(index);
+
+	for (i = 0; i < sp; i++) {
+		if (infinite[i] == tc) {
+			printf("Loops %d %d\n", stack[i], jumps[stack[i]]);
+			break;
+		}
+	}
+	if (i == sp)
+		printf("Loops %d %d\n", stack[sp - 1], jumps[stack[sp - 1]]);
 }
 
 void init(void)
@@ -131,6 +196,7 @@ void init(void)
 	}
 	for (int i = 0; i < csize; i++) {
 		scanf(" %c", &codes[i]);
+		m_for_codes[i] = 0;
 	}
 	getchar();
 	for (int i = 0; i < isize; i++) {
@@ -141,9 +207,8 @@ void init(void)
 
 int main(void)
 {
-	int tc;
 	scanf("%d", &tc);
-	for (int t = 0; t < tc; t++) {
+	for (int t = 1; t <= tc; t++) {
 
 		/* get input & init arrays */
 		init();
