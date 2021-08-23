3f8de8f40dbd8058f5e113facb316780182cf26d
BY1994 <lovelychoco100@gmail.com>
2021-08-23 23:07:48 +0900

    [Algorithm] Baekjoon: update 3954 brain f__k (PASS!!)
    This patch updated 3954 brain f**k interpreter problem.
    This patch got success. It was due to wrong data type.
    
    Signed-off-by: BY1994 <lovelychoco100@gmail.com>

--- Algorithm/Baekjoon/3954_Brainf__k_interpreter/3954_Brainf__k_interpreter/main.c
+++ Algorithm/Baekjoon/3954_Brainf__k_interpreter/3954_Brainf__k_interpreter/main.c
@@ -68,7 +68,29 @@ Loops 3 6
 
 6. 모든 반례 적용하고도 wrong
 5번에서 무한루프가 항상 조건이 똑같지 않다는게 문제인 듯...
+가장 안쪽 루프 말고 중간 루프가 답이도록 반례 만들어보기
+1억번째에 우연히 안쪽 루프에 있도록
 
+내가 만든 내 코드 반례
+1
+8 23 1
+++[[[>>>>>>]<<<<<<++]+]
+a
+
+4+17*n 하면 100000001번째에 10 번째 코드 (>) 실행 중
+답
+Loops 3 20
+=> 일반적인 풀이인 min, max 범위 적용
+https://kibbomi.tistory.com/6
+
+7. 자료형 지정 오류
+그래도 틀렸는데, unsigned char 하면 형변환이 값을 이상하게 바꿨나 싶어서
+int로 다 바꿨다.
+
+=> 통과!
+(memory[pointer] + 256 - 1) % 256; 이런 부분에서 형변환이 문제를 일으키는 줄 알았는데,
+확인 결과 jumps[] 배열에 들어가는 값이 0~4096 index 인데,
+이걸 unsigned char 로 지정해버려서 문제가 되었다.
 */
 
 #include <stdio.h>
@@ -76,17 +98,17 @@ Loops 3 6
 int msize;
 int csize;
 int isize;
-unsigned int pointer;
-unsigned int input_index;
+int pointer;
+int input_index;
 
 unsigned char codes[4096];
-unsigned char m_for_codes[4096];
 unsigned char inputs[4096];
 unsigned char memory[100010];
-unsigned char jumps[4096];
+int jumps[4096]; // 실수한 지점, 이건 4096 index 까지 들어가야하는데 unsigned char 로 잘못 지정함
 
 int stack[5000];
-int infinite[5000];
+int stack_pos[5000];
+int cptr;
 int sp;
 int tc;
 
@@ -127,7 +149,7 @@ int do_code(int index)
 			ret = jumps[index] + 1;
 		}
 		else {
-			infinite[sp] = 0;
+			stack_pos[sp] = cptr;
 			stack[sp] = index;
 			sp++;
 		}
@@ -135,11 +157,9 @@ int do_code(int index)
 	else if (codes[index] == ']') {
 		if (memory[pointer] != 0) {
 			ret = jumps[index] + 1;
-			/* same as previous enter */
-			if (m_for_codes[index] == memory[pointer]) infinite[sp-1] = tc;
-			else infinite[sp-1] = 0;
+			stack_pos[sp - 1] = cptr; /* update */
 		}
-		else sp--;
+		else sp--; /* loop end */
 	}
 	else if (codes[index] == ',') {
 		if (input_index >= isize) {
@@ -149,7 +169,6 @@ int do_code(int index)
 			memory[pointer] = inputs[input_index++];
 		}
 	} // pass "."
-	m_for_codes[index] = memory[pointer];
 
 	return ret;
 }
@@ -163,6 +182,7 @@ void run(void)
 	pointer = 0;
 	input_index = 0;
 	sp = 0;
+	cptr = 0;
 
 	/* terminates within 50 000 000 instructions */
 	while (max_code--) {
@@ -171,20 +191,22 @@ void run(void)
 			return;
 		}
 		index = do_code(index);
+		cptr++;
 	}
 
 	/* at most 50 000 000 instructions within infinite loop */
 	max_code = 50000001;
-	while (max_code--) index = do_code(index);
+	while (max_code--) {
+		index = do_code(index);
+		cptr++;
+	}
 
 	for (i = 0; i < sp; i++) {
-		if (infinite[i] == tc) {
+		if (cptr-1-stack_pos[i] <= 50000000) {
 			printf("Loops %d %d\n", stack[i], jumps[stack[i]]);
 			break;
 		}
 	}
-	if (i == sp)
-		printf("Loops %d %d\n", stack[sp - 1], jumps[stack[sp - 1]]);
 }
 
 void init(void)
@@ -196,7 +218,6 @@ void init(void)
 	}
 	for (int i = 0; i < csize; i++) {
 		scanf(" %c", &codes[i]);
-		m_for_codes[i] = 0;
 	}
 	getchar();
 	for (int i = 0; i < isize; i++) {
