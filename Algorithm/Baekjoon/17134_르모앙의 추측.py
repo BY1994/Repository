93f6bdcd098befb99c055c275fe8a18229d422e7
BY1994 <lovelychoco100@gmail.com>
2021-09-30 21:46:05 +0900

    [Algorithm] Baekjoon 17134 problem about prime number (fail)
    This patch added Baekjoon 17134 problem.
    It didn't get accept yet.
    
    Signed-off-by: BY1994 <lovelychoco100@gmail.com>

--- Algorithm/Baekjoon/17134_르모앙의 추측.py
+++ Algorithm/Baekjoon/17134_르모앙의 추측.py
@@ -2,8 +2,53 @@
 17134 르모앙의 추측
 
 2019.04.10 PBY 최초작성
+이분탐색 코드
+https://velog.io/@madfinger/Binary-Search%EC%9D%B4%EC%A7%84-%ED%83%90%EC%83%89-%ED%8C%8C%EC%9D%B4%EC%8D%AC
+
+2021.09.30 PBY
+찾아보니 FFT 알고리즘 문제라고 한다.
+FFT 알고리즘 공부 후에 다시 풀이 필요
 """
 
+# 규칙성 찾아서 꼼수 부리려고 했는데 아래 방법은 안 통했다. (규칙성 틀림)
+"""
+# 소수 미리 찾아두기
+check = [False] * 1000001
+check[0] = True; check[1] = True
+primes = []
+
+# 에라토스테네스의 체
+for start in range(2, 1000001):
+    for cae in range(start+start, 1000001, start):
+        check[cae] = True # 거르기
+
+for start in range(2, 1000001):
+    if check[start] == False:
+        primes.append(start)
+        
+for _ in range(int(input())):
+    N = int(input())
+    target = (N-3)//2
+    
+    left = 0 
+    right = len(primes)-1
+
+    ans = 0
+    while left<=right:
+        mid = (left+right)//2
+        if primes[mid] == target:
+            ans = mid
+            break
+        elif primes[mid]>target:
+            right = mid-1
+            ans = right
+        else :
+            left = mid+1
+
+    print(ans + 1) # index 가 곧 개수 (2 ~ 지금 이분탐색으로 찾은 홀수...)
+"""
+
+"""
 #@profile
 def main():
     
@@ -34,6 +79,7 @@ def main():
   
 if __name__ == "__main__":
     main()
+"""
 
 # 시간초과
 """
