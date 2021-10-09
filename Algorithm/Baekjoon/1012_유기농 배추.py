21f397c5bee84fd518e3c9dc331d3ae61780272c
BY1994 <lovelychoco100@gmail.com>
2021-10-09 16:41:37 +0900

    [Algorithm] Baekjoon 1012 problem (DFS)
    This patch added Baekjoon 1012 problem about DFS, BFS.
    
    Signed-off-by: BY1994 <lovelychoco100@gmail.com>

--- Algorithm/Baekjoon/1012_유기농 배추.py
+++ Algorithm/Baekjoon/1012_유기농 배추.py
@@ -29,12 +29,115 @@ output)
 1
 
 2019.09.19 PBY
+
+2021.10.09 예전 코드의 문제를 확인하여 제출
 """
+
+# 시간 줄이기 위해 한 번 더
+# 굳이 필요없는 거 다 제거 => 그래도 344 ms...
+import sys
+sys.setrecursionlimit(10**6)
+
+# dfs
+def findvege(i, j):
+    # 상하좌우
+    for nx, ny in ((i-1, j), (i, j-1), (i+1, j), (i, j+1)):
+        if 0 <= nx < M and 0 <= ny < N and bat[nx][ny] == 1:
+            bat[nx][ny] = 0
+            findvege(nx, ny)
+
+# input
+for testcase in range(int(input())):
+    ans = 0
+    M, N, K = map(int, input().split())
+    bat = [[0]*N for _ in range(M)]
+    for row in range(K):
+        x, y = map(int, input().split())
+        bat[x][y] = 1
+
+    # main
+    for i in range(M):
+        for j in range(N):
+            if bat[i][j] == 1:
+                ans += 1
+                bat[i][j] = 0
+                findvege(i, j)
+
+    print(ans)
+
+# ;;; bat를 계속 넘기니까 메모리 초과가 나지...
+"""
+import sys
+sys.setrecursionlimit(10**6)
+
+# dfs
+def findvege(i, j):
+    global bat, M, N, K
+    # 상하좌우
+    for nx, ny in ((i-1, j), (i, j-1), (i+1, j), (i, j+1)):
+        if 0 <= nx < M and 0 <= ny < N and bat[nx][ny] == 1:
+            K -= 1
+            bat[nx][ny] = 0
+            findvege(nx, ny)
+
+# input
+for testcase in range(int(input())):
+    ans = 0
+    M, N, K = map(int, input().split())
+    bat = [[0]*N for _ in range(M)]
+    for row in range(K):
+        x, y = map(int, input().split())
+        bat[x][y] = 1
+
+    # main
+    while K > 0:
+        for i in range(M):
+            for j in range(N):
+                if bat[i][j] == 1:
+                    ans += 1
+                    K -= 1
+                    bat[i][j] = 0
+                    findvege(i, j)
+
+    print(ans)
+"""
+
+# 64 ms 나온 코드 참고
+"""
+import sys;p=sys.stdin.readline;
+sys.setrecursionlimit(1000000)
+q=int(p())
+def T(t, o, s):
+    t[s][o]=0
+    if o+1 < x and t[s][o+1]==1:
+        T(t,o+1,s)
+    if o-1>= 0 and t[s][o-1]==1:
+        T(t, o-1,s)
+    if s -1 >= 0 and t[s-1][o]==1:
+        T(t, o,s-1)
+    if s +1 < y and t[s+1][o]==1:
+        T(t,o,s+1)
+
+for _ in range(q):
+    x, y, c = map(int, p().split())
+    t = [[0] * x for _ in range(y)]
+    for i in range(0,c):
+        m,n=map(int,p().split());t[n][m] = 1
+    v = 0
+    for i in range(0,x):
+        for j in range(0, y):
+            if t[j][i] == 1:
+                T(t, i, j);v+=1
+    print(v)
+"""
+
+# 메모리 초과
 
 ################################
 # 계속 틀렸던 이유는 여기!!!!!!! => 런타임 에러 해결
 # https://www.acmicpc.net/board/view/37591
 ################################
+"""
 import sys
 sys.setrecursionlimit(10**6)
 
@@ -69,6 +172,7 @@ for testcase in range(int(input())):
                     findvege(i, j, M, N, bat)
 
     print(ans)
+"""
 
 # 런타임 에러
 """
