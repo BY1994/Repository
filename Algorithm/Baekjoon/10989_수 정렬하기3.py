681aeec17e08d6b84aa8bbfeeda54c20ee6fd9ce
BY1994 <lovelychoco100@gmail.com>
2021-10-11 23:11:41 +0900

    [Algorithm] Baekjoon: 10989 (Counting Sort)
    This patch added Baekjoon 10989 problem
    about Counting Sort.
    
    Signed-off-by: BY1994 <lovelychoco100@gmail.com>

--- Algorithm/Baekjoon/10989_수 정렬하기3.py
+++ Algorithm/Baekjoon/10989_수 정렬하기3.py
@@ -1,3 +1,36 @@
+"""
+이 문제는 개수가 엄청 많이 들어오기 때문에 일반적인 정렬 방법으로는 못 푼다.
+범위가 한정되어있기 때문에 카운팅 소트가 맞다
+
+2020년 풀이 시도 => 메모리 초과
+2021년 문제 파악
+"""
+import sys
+
+counting = [0]*10001
+t = int(sys.stdin.readline())
+for _ in range(t):
+	counting[int(sys.stdin.readline())]+=1
+
+for i in range(10001):
+        for j in range(counting[i]):
+                sys.stdout.write(str(i) + '\n')
+
+# 시간초과 => 헉 질문답안을 보니 print 를 쓰면 시간초과라고 한다.
+"""
+counting = [0]*10001
+t = int(input())
+for _ in range(t):
+	counting[int(input())]+=1
+
+for i in range(10001):
+        for j in range(counting[i]):
+                print(i)
+"""
+
+# 메모리 초과 => 원인 print 를 * 해서 문자열로 만든 다음에 하니까
+# 문자열이 차지하는 공간이 많았던 것
+"""
 counting = [0]*10001
 t = int(input())
 for _ in range(t):
@@ -6,3 +39,4 @@ for _ in range(t):
 for i in range(10001):
 	if (counting[i]):
 		print(f"{i}\n"*counting[i], end='')
+"""
