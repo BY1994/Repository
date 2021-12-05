0063b6c4ef57ea5b5a2e3ca6c15a6f620a26eba3
BY1994 <lovelychoco100@gmail.com>
2021-12-05 20:07:18 +0900

    [Algorithm] Baekjoon: 2109 greedy (not success)
    This patch added 2109 greedy problem.
    Wrong Answer.
    
    Signed-off-by: BY1994 <lovelychoco100@gmail.com>

---
+++ Algorithm/Baekjoon/2109_순회강연.py
@@ -0,0 +1,183 @@
+"""
+2109 순회강연
+
+문제 입력
+7
+20 1
+2 1
+10 3
+100 2
+8 2
+5 20
+50 10
+출력 185
+
+인덱스 에러는 0 입력을 처리 안 함
+
+3
+1 1
+10 2
+10 2
+내 답 11
+정답 20
+
+내가 만든 반례
+3
+70 3
+60 3
+50 3
+내 답 70
+정답 180
+
+4
+70 3
+60 3
+50 3
+40 3
+오답 220
+정답 180
+
+4
+20 2
+30 2
+40 3
+40 3
+오답 80
+정답 110
+"""
+
+N = int(input())
+pos = []
+for i in range(N):
+    pos.append(list(map(int, input().split())))
+
+ans = 0
+
+if len(pos):
+    pos.sort(key = lambda x: (-x[1], -x[0]))
+    mtemp = 0
+    cur = pos[0][1]
+    i = 0
+    while(i < N and cur > 0):
+#        print("###i ", i)
+#        print(pos)
+        if pos[i][1] < cur:
+#            print("###", mtemp)
+            ans += pos[mtemp][0]
+            cur = pos[i][1]
+            mtemp += 1
+            i = mtemp
+        else:
+#            pos[i][1] -= 1
+            if pos[mtemp][0] < pos[i][0]:
+                mtemp = i
+            if i == N-1:
+                ans += pos[mtemp][0]
+                cur -= 1
+                if pos[i][1] > cur:
+                    i = mtemp
+                    mtemp += 1
+            i += 1
+
+
+print(ans)
+
+# 반례
+# https://www.acmicpc.net/board/view/68574
+"""
+N = int(input())
+pos = []
+for i in range(N):
+    pos.append(list(map(int, input().split())))
+
+ans = 0
+
+if len(pos):
+    pos.sort(key = lambda x: (-x[1], -x[0]))
+    mtemp = 0
+    cur = pos[0][1]
+    i = 0
+    while(i < N):
+#        print("###i ", i)
+#        print(pos)
+        if pos[i][1] < cur:
+#            print("###", mtemp)
+            ans += pos[mtemp][0]
+            cur = pos[i][1]
+            mtemp += 1
+            i = mtemp
+        else:
+            pos[i][1] -= 1
+            if pos[mtemp][0] < pos[i][0]:
+                mtemp = i
+            if i == N-1:
+                ans += pos[mtemp][0]
+#                cur -= 1
+                if pos[i][1] > 1:
+                    i = mtemp
+                    mtemp += 1
+            i += 1
+
+
+print(ans)
+"""
+
+
+# cur 값을 하루씩 적게 만들면서 돌아야하는데, 그게 적용 안 됨...
+"""
+N = int(input())
+pos = []
+for i in range(N):
+    pos.append(list(map(int, input().split())))
+
+ans = 0
+
+if len(pos):
+    pos.sort(key = lambda x: (-x[1], -x[0]))
+    mtemp = 0
+    cur = pos[0][1]
+    i = 0
+    while(i < N):
+#        print("###i ", i)
+        if pos[i][1] < cur:
+#            print("###", mtemp)
+            ans += pos[mtemp][0]
+            cur = pos[i][1]
+            mtemp += 1
+            i = mtemp
+        else:
+            if pos[mtemp][0] < pos[i][0]:
+                mtemp = i
+            i += 1
+
+    ans += pos[mtemp][0]
+print(ans)
+"""
+
+# 반례
+# https://www.acmicpc.net/board/view/1885
+
+"""
+N = int(input())
+pos = []
+for i in range(N):
+    pos.append(list(map(int, input().split())))
+
+ans = 0
+
+if len(pos):
+    pos.sort(key = lambda x: (x[1], -x[0]))
+    mtemp = 0
+    cur = pos[0][1]
+    for i in range(N):
+        if pos[i][1] > cur:
+            ans += pos[mtemp][0]
+            cur = pos[i][1]
+            mtemp = i
+        else:
+            if pos[mtemp][0] < pos[i][0]:
+                mtemp = i
+
+    ans += pos[mtemp][0]
+print(ans)
+"""
