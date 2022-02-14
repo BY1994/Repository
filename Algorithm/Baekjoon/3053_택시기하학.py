a6d0ad4a87879a7793cbe93c5159d32a0f9e0152
BY1994 <lovelychoco100@gmail.com>
2022-02-14 23:34:59 +0900

    [Algorithm] Baekjoon: 3053 problem (geometry)
    This patch added Baekjoon 3053
    which is about geometry.
    
    Signed-off-by: BY1994 <lovelychoco100@gmail.com>

---
+++ Algorithm/Baekjoon/3053_택시기하학.py
@@ -0,0 +1,15 @@
+"""
+3053 택시 기하학
+
+원의 넓이
+마름모 넓이
+"""
+
+R = int(input())
+print("{:.6f}".format(R*R*3.14159265359))
+print("{:.6f}".format(R*R*2))
+
+
+# https://www.acmicpc.net/source/21632054
+# main(a){scanf("%d",&a);printf("%f %f",a*a*acos(-1),a*a*2.);}
+# cos 을 그렸을 때 -1 이 나오는 지점이 파이!
