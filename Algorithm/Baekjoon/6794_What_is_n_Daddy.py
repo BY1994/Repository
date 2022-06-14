6d44eacbb201d07ec6e4cad74195bbcfc9c5b1d3
BY1994 <lovelychoco100@gmail.com>
2022-06-14 23:01:04 +0900

    [Algorithm] Baekjoon: 6794 preprocessing
    This patch added Baekjoon 6794 preprocessing.
    
    Signed-off-by: BY1994 <lovelychoco100@gmail.com>

---
+++ Algorithm/Baekjoon/6794_What_is_n_Daddy.py
@@ -0,0 +1,25 @@
+"""
+6794 What is n, Daddy?
+
+preprocessing
+
+1
+2 / 1 1
+3 / 2 1
+4 / 3 1 / 2 2
+5 / 4 1 / 3 2
+5 1 / 4 2 / 3 3
+5 2 / 4 3
+5 3 / 4 4
+5 4
+5 5
+"""
+
+ans = [0, 1, 2, 2, 3, 3, 3, 2, 2, 1, 1]
+print(ans[int(input())])
+
+# https://www.acmicpc.net/source/22421084
+"""
+n=int(input())
+print(1+(9>n>1)+(7>n>3))
+"""
