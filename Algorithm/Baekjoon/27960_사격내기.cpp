eedd7051360d3c4ed98980bd40c09a1526db30df
BY1994 <lovelychoco100@gmail.com>
2023-05-05 19:36:08 +0900

    [Algorithm] Baekjoon: 27960 bit masking
    This patch added Baekjoon 27960 bitmasking.

---
+++ Algorithm/Baekjoon/27960_사격내기.cpp
@@ -0,0 +1,17 @@
+/*
+27960 사격 내기 
+\n 필요 없음! 48 B 가 1등이고, 아래 내 코드는 \n 때문에 50 B 
+*/
+// 제출용 숏코딩
+main(A,B){scanf("%d%d",&A,&B);printf("%d\n",A^B);} 
+// main(A,B){scanf("%d%d",&A,&B);printf("%d",A^B);} 
+
+#if 0 
+#include <stdio.h>
+int main(void) {
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d\n", A^B);
+	return 0;
+}
+#endif
